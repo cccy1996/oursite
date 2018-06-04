debb39c8babd578f71c9e694fc4f4d55228460fb
elin <85221742@qq.com>
2018-06-04 19:52:09 +0800

    修改了buy中的Transaction

--- our_site/buy/models.py
+++ our_site/buy/models.py
@@ -5,7 +5,11 @@ from display.models import Composition
 # Create your models here.
 class Transaction(models.Model):
     trans_id = models.AutoField(primary_key=True)
+    app_date = models.DateField('application occured date')
     trans_date = models.DateTimeField('transaction occured date')
     trans_user = models.ForeignKey(Commuser_relation, on_delete = False)
     trans_composition = models.ForeignKey(Composition, on_delete = False)
     trans_price = models.DecimalField(max_digits=9,decimal_places=1)
+    is_accept = models.NullBooleanField()
+    is_paid = models.NullBooleanField()
+
