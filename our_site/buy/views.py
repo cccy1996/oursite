cbfc04a7b430867d725e92995dce7b12da081267
elin <85221742@qq.com>
2018-06-05 19:25:45 +0800

    update gitignore

--- our_site/buy/views.py
+++ our_site/buy/views.py
@@ -5,6 +5,8 @@ from django.http.response import HttpResponse
 from django.utils import timezone
 
 def transact(request, pk):
+    pass
+    '''
     try:
         require_composition = Composition.objects.get(pk = pk)
     except Exception:
@@ -16,3 +18,4 @@ def transact(request, pk):
     new_transaction.trans_price = require_composition.price
     new_transaction.save()
     return render(request, 'buy/transact.html', {'new_transaction' : new_transaction})
+    '''
