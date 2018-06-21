f46a08b3a72909e164865b421b385d3e6c7017b2
ly <1315374322@qq.com>
2018-06-21 16:09:37 +0800

    加入参数id

--- our_site/display/urls.py
+++ our_site/display/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = 'display'
 
 urlpatterns = [
-    path('expert_detail/',views.expert_detail,name='expert_detail'),
+    path('expert_detail/<int:id>',views.expert_detail,name='expert_detail'),
     path('paper_detail/<slug:id>/',views.paper_detail,name='paper_detail')
 ]
